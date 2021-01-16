081a72fd5293b3e01aa1bb9ef2183fa11b581d23
QuanDo <thienkimlove@gmail.com>
2021-01-16 07:29:16 +0700

    update

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ EncodeCoordinatesParams = collections.namedtuple('EncodeCoordinatesParams', [
 def _dict_to_array(id_to_char, default_character):
   num_char_classes = max(id_to_char.keys()) + 1
   array = [default_character] * num_char_classes
-  for k, v in id_to_char.iteritems():
+  for k, v in id_to_char.items():
     array[k] = v
   return array
 
@@ -532,7 +532,7 @@ class Model(object):
                    streaming=True,
                    rej_char=self._params.null_code))
 
-      for name, value in names_to_values.iteritems():
+      for name, value in names_to_values.items():
         summary_name = 'eval/' + name
         tf.summary.scalar(summary_name, tf.Print(value, [value], summary_name))
       return names_to_updates.values()
---
+++ test.py
@@ -0,0 +1,4 @@
+for i in range(8):
+    test = '/content/drive/MyDrive/datasets/samples/ocr/crops/%d.png' % i
+
+    print(test)
